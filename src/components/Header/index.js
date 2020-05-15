3dd1174761f97cbf1819ec6810b9a075860bc287
Gabriel Lima <grl@icomp.ufam.edu.br>
2020-05-15 15:36:23 -0400

    changed font-size unit in Comment component from pixel to rem, changed facebook logo from png to svg and added media querie

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import facebookLogo from '../../assets/fb-logo.png';
+import facebookLogo from '../../assets/fb-logo.svg';
 import userLogo from '../../assets/user-circle-solid.svg';
 import './Header.css'
 function Header() {
